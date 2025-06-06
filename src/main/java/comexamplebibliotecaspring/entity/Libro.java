92ac96cceb87b033c94afaa0d193078ecc3a3197
USER <cristhian20023@gmail.com>
2025-06-05 20:47:30 -0500

    creando la clase libro

---
+++ src/main/java/comexamplebibliotecaspring/entity/Libro.java
@@ -0,0 +1,19 @@
+package com.example.bibliotecaspring.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "libros")
+@Data
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int añoPublicacion;
+    private boolean disponible;
+}
