7b51412b60408cf1195c2c6aa83c8da383ef5c92
USER <cristhian20023@gmail.com>
2025-06-05 20:47:30 -0500

    Primer commit

---
+++ src/main/java/org/example/bibliotecaspring/BibliotecaSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.bibliotecaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaSpringApplication.class, args);
+    }
+
+}
